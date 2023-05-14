271bec955a57df9066c78e28954da80739e3e840
natsumi10 <natsumi.k1234@gmail.com>
2023-05-14 13:15:32 +0900

    continue to study.

--- path_changer.py
+++ path_changer.py
@@ -1,10 +1,15 @@
-from PySide6.QtWidgets import QApplication, QWidget, QDialog, QLineEdit, QPushButton
-
+import PySide6
+from PySide6 import QtCore
+from PySide6 import QtWidgets
+import os
 import sys
 
-app = QApplication(sys.argv)
+dirname = os.path.dirname(PySide6.__file__)
+plugin_path = os.path.join(dirname, 'plugins', 'platforms')
+os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
 
-window = QWidget()
-window.show()
-
-app.exec_()
+app = QtWidgets.QApplication()
+label = QtWidgets.QLabel("Hello", alignment=QtCore.Qt.AlignCenter)
+label.setStyleSheet("font-size:18px;")
+label.show()
+sys.exit(app.exec())
