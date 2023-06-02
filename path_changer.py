63f829f236808f30d4a12704750a837b5701c8ac
natsumi10 <natsumi.k1234@gmail.com>
2023-06-03 01:00:26 +0900

    restart pyside 6 with the tutorial made by pythonGUIS

--- path_changer.py
+++ path_changer.py
@@ -1,31 +1,32 @@
-import PySide6
-from PySide6 import QtCore, QtWidgets, QApplication
+#import PySide6
+from PySide6 import QtWidgets
+from PySide6.QtWidgets import QApplication
+
+
 
 import os
 import sys
 
 # PySide main app class. Inherit windows class.
-class MainWindow_class(QWidget):
+class MainWindow_class(QtWidgets.QWidget):
     # parent is used when you another window.
-    def __init__(self, paret=None):
+    def __init__(self, parent=None):
         # init Base class. In this case it's about QWidget
         super().__init__(parent)
 
 if __name__ == "__main__":
-    # Register PySide6 to env variables
-    dirname = os.path.dirname(PySide6.__file__)
-    plugin_path = os.path.join(dirname, 'plugins', 'platforms')
-    os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
-    
-    print ("sys.argv is : ", sys.argv)
-    
-    # Execute PySide6
+
+    # Pass is sys.argv to allow command line arguments for your app.
+    # If you know you won't use command line arguments  QApplication([]) works too.
     app = QApplication(sys.argv)
+    
+    # Create a Qt widget, which will be our window.
     window = MainWindow_class()
     # Show windows command for PySide6 window.
+    # Windows are hidden by default
     window.show()
     sys.exit(app.exec())
-
+    
     """
     label = QtWidgets.QLabel("Hello", alignment=QtCore.Qt.AlignCenter)
     label.setStyleSheet("font-size:128px;")
