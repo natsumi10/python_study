b64757356cbcab196aa8b0e9d73e5b4f630e0882
natsumi10 <natsumi.k1234@gmail.com>
2023-06-18 00:11:49 +0900

    first commit

--- study_gui.py
+++ study_gui.py
@@ -1,2 +1,24 @@
+import sys
+from PySide6 import QtGui
+from PySide6 import QtWidgets
+from PySide6.QtWidgets import QApplication
+
+class MainWindow(QtWidgets.QMainWindow):
+    print ("start")
+    def __init__(self,parent=None):
+        super(MainWindow,self).__init__(parent=parent)
+        self.__initialize()
+    
+    def __initialize(self):
+        center = QtWidgets.QWidget()
+        layout = QtWidgets.QVBoxLayout()
+        center.setLayout(layout)
+        self.setCentralWidget(center)
+
+
 if __name__ == "__main__":
     print ("start")
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
