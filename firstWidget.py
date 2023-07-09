0758fcfd4bd62bcff5af73016a96e5aeafa66ffa
natsumi10 <natsumi.k1234@gmail.com>
2023-07-09 13:19:07 +0900

    Fix program.

--- firstWidget.py
+++ firstWidget.py
@@ -1,10 +1,12 @@
-import re
+import re, sys
 from PySide6 import QtWidgets
+from PySide6 import QtCore
 
 
 class FirstItemsTable(QtWidgets.QTableView):
     # 先頭から「file:/」
-    RemovePreferLocal = re.compile("^file[:][/]+")
+    # 今は使っていないので一次的にコメントアウト
+    # RemovePreferLocal = re.compile("^file[:][/]+")
 
     def __infit__(self, parent=None):
         super(FirstItemsTable, self).__init__(parent=parent)
@@ -21,4 +23,17 @@ class FirstWidget(QtWidgets.QWidget):
     def __initialize(self):
         layout = QtWidgets.QVBoxLayout()
         self.setLayout(layout)
+
+        self.__test1_radio = QtWidgets.QRadioButton("Test1")
+        self.__test2_radio = QtWidgets.QRadioButton("Test2")
+        self.__test1_radio.setChecked(True)
+
+        radio_layout = QtWidgets.QHBoxLayout()
+        radio_layout.addWidget(self.__test1_radio)
+        radio_layout.addWidget(self.__test2_radio)
+
+        box = QtWidgets.QGroupBox()
+        box.setLayout(radio_layout)
+
+        self.__first_table = FirstItemsTable()
 
